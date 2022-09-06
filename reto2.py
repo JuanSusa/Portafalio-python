6ada20ffc74d2c404601a66a5e7673d090a93e35
JuanSusa <kmilosusasusa@gmail.com>
2022-09-06 14:35:31 -0300

    ejercicios python 1

--- reto2.py
+++ reto2.py
@@ -2,3 +2,9 @@ pesoBebe=int(input("cual es el peso del bebe "))
 edadBebe=int(input("cual es la edad del bebe "))
 
 ##calculamos la dosis a aplicar##
+peso=pesoBebe+10
+edad=edadBebe*10
+
+dosisVacuna=pesoBebe/edadBebe*8
+
+print("La dosis a aplicar es ", dosisVacuna)
